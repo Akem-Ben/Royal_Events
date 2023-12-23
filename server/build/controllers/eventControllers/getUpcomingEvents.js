2f3f3d4fad3d66a2a9162307aebd498f9ad91618
Akem-Ben <akemini.ndaobong@gmail.com>
2023-12-23 12:56:56 +0100

    changes

--- server/build/controllers/eventControllers/getUpcomingEvents.js
+++ server/build/controllers/eventControllers/getUpcomingEvents.js
@@ -7,40 +7,6 @@ exports.getUpcomingEvents = void 0;
 const sequelize_1 = require("sequelize");
 const eventModel_1 = __importDefault(require("../../models/eventModel/eventModel"));
 const helpers_1 = require("../../helpers/helpers");
-// export const getUpcomingEvents = async (request: Request, response: Response) => {
-//     try {
-//         const presentDay = new Date()
-//         let getUpcomingEvents:any = await Event.findAll({ where: { event_start_date: {
-//             [Op.gt]: presentDay
-//           }, 
-//         isBlocked: false
-//         } })
-//         if (getUpcomingEvents.length === 0) {
-//             return response.status(404).json({
-//                 message: `No Upcoming Events found`
-//             })
-//         }
-//         getUpcomingEvents = getUpcomingEvents.map((event:any)=>{
-//         return{
-//             ...event,
-//             event_start_date: convertToDDMMYY(event.event_start_date),
-//             event_end_date: convertToDDMMYY(event.event_end_date)
-//         }
-//         })
-//         return response.status(200).json({
-//             status: 'Success',
-//             method: request.method,
-//             message: `Upcoming events found successfully`,
-//             data: getUpcomingEvents
-//         })
-//     } catch (error: any) {
-//         response.status(400).json({
-//             status: 'error',
-//             method: request.method,
-//             message: 'Error'
-//         })
-//     }
-// }
 const getUpcomingEvents = async (request, response) => {
     try {
         const presentDay = new Date();
--- server/src/controllers/eventControllers/getUpcomingEvents.ts
+++ server/src/controllers/eventControllers/getUpcomingEvents.ts
@@ -3,44 +3,6 @@ import { Op } from 'sequelize'
 import Event from '../../models/eventModel/eventModel'
 import { convertToDDMMYY, convertToISODateString } from "../../helpers/helpers";
 
-// export const getUpcomingEvents = async (request: Request, response: Response) => {
-//     try {
-//         const presentDay = new Date()
-
-//         let getUpcomingEvents:any = await Event.findAll({ where: { event_start_date: {
-//             [Op.gt]: presentDay
-//           }, 
-//         isBlocked: false
-//         } })
-
-//         if (getUpcomingEvents.length === 0) {
-//             return response.status(404).json({
-//                 message: `No Upcoming Events found`
-//             })
-//         }
-//         getUpcomingEvents = getUpcomingEvents.map((event:any)=>{
-//         return{
-//             ...event,
-//             event_start_date: convertToDDMMYY(event.event_start_date),
-//             event_end_date: convertToDDMMYY(event.event_end_date)
-//         }
-//         })
-//         return response.status(200).json({
-//             status: 'Success',
-//             method: request.method,
-//             message: `Upcoming events found successfully`,
-//             data: getUpcomingEvents
-//         })
-
-//     } catch (error: any) {
-//         response.status(400).json({
-//             status: 'error',
-//             method: request.method,
-//             message: 'Error'
-//         })
-//     }
-// }
-
 export const getUpcomingEvents = async (request: Request, response: Response) => {
     try {
       const presentDay = new Date();
